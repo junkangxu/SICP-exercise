68244097ddcd844e160ca30af5cf55306639958f
junkangxu <david.xujunkang@gmail.com>
2017-12-30 12:44:01 +0800

    12/30/17 chapter 1.3 finished

--- src/chapter1/1.3.scm
+++ src/chapter1/1.3.scm
@@ -113,3 +113,196 @@
 ;;;;;;;;;;
 (define (f g) (g 2))
 ; (f f) => (f 2) => (2 2) => Error, 2 is not a function
+
+
+
+;;;;;;;;;;
+;;;Ex 1.35
+;;;;;;;;;;
+; x -> 1 + 1/x
+; => multiply both side by x x^2 -> x + 1, which is the definition of phi
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+; result -> 1.618
+
+
+
+;;;;;;;;;;
+;;;Ex 1.36
+;;;;;;;;;;
+(define tolerance 0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+      (display guess)
+      (newline)
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+            next
+            (try next))))
+    (try first-guess))
+
+(define (x-to-the-x y)
+  (fixed-point (lambda (x) (/ (log y) (log x)))
+    1000))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.37
+;;;;;;;;;;
+; a)
+(define (cont-frac n d k)
+  (cond ((= k 0) 0)
+        (else (/ (n k) (+ (d k) (cont-frac n d (- k 1)))))))
+
+; b)
+(define (cont-frac n d k)
+  (define (iter a result)
+    (if (= a 0)
+        result
+        (iter (- a 1)
+              (/ (n a) (+ (d a) result)))))
+  (iter k 0))
+; both method need 10 terms to keep error less than 0.0001
+
+
+
+;;;;;;;;;;
+;;;Ex 1.38
+;;;;;;;;;;
+(define (eular k)
+  (+ 2.0 (cont-frac (lambda (x) 1)
+                    (lambda (x)
+                      (if (= (remainder x 3) 2)
+                          (/ (+ i 1) 1.5)
+                          1))
+                    k)))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.39
+;;;;;;;;;;
+(define (tan-cf x k)
+  (define (iter a result)
+    (if (= a 0)
+        result
+        (iter (- a 1)
+              (/ (if (= a 1) x (square x))
+                 (- (- (* 2 a) 1)
+                 result)))))
+  (iter k 0) )
+
+
+
+;;;;;;;;;;
+;;;Ex 1.40
+;;;;;;;;;;
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x)
+       (* a (square x))
+       (* b x)
+       c)))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.41
+;;;;;;;;;;
+(define (double f)
+  (lambda (x) (f (f x))))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.42
+;;;;;;;;;;
+(define (compose f g x)
+  (lambda (x) (f (g x))))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.43
+;;;;;;;;;;
+(define (repeat f n)
+  (if (< n 1)
+      (lambda (x) x)
+      (compose f (repeat f (- n 1)))))
+
+
+;;;;;;;;;;
+;;;Ex 1.44
+;;;;;;;;;;
+(define dx 0.00001)
+
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx)))
+       3 )))
+
+(define (n-fold-smooth f n) ((repeated smooth n) f))
+
+
+
+;;;;;;;;;;
+;;;Ex 1.45
+;;;;;;;;;;
+(define (get-max-pow n)
+   (define (iter p r)
+     (if (< (- n r) 0)
+         (- p 1)
+         (iter (+ p 1) (* r 2))))
+
+   (iter 1 2))
+
+ (define (pow b p)
+   (define (even? x)
+     (= (remainder x 2) 0))
+
+   (define (sqr x)
+     (* x x))
+
+   (define (iter res a n)
+     (if (= n 0)
+         res
+         (if (even? n)
+             (iter res (sqr a) (/ n 2))
+             (iter (* res a) a (- n 1)))))
+
+   (iter 1 b p))
+
+ (define (nth-root n x)
+   (fixed-point ((repeated average-damp (get-max-pow n))
+                 (lambda (y) (/ x (pow y (- n 1)))))
+                1.0))
+
+
+
+(define (close-enough? v1 v2)
+  (define tolerance 1.e-6)
+  (< (/ (abs (- v1 v2)) v2)  tolerance))
+
+(define (iterative-improve improve close-enough?)
+  (lambda (x)
+    (let ((xim (improve x)))
+      (if (close-enough? x xim)
+          xim
+          ((iterative-improve improve close-enough?) xim))
+    )))
+; a) rewrite sqrt using iterative-improve
+(define (sqrt x)
+  ((iterative-improve
+    (lambda (y)
+      (/ (+ (/ x y) y) 2))
+    close-enough?) 1.0))
+; b) rewrite fixed-point using iterative-improve
+(define (fixed-point f first-guess)
+  ((iterative-improve 
+    f
+    close-enough?) first-guess))
